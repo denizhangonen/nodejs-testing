bd3b0b10ef1b94d6168f52130d4eb44107d70c5d
Denizhan Gönen <denizhangonen@Denizhans-MacBook-Pro.local>
2019-11-10 22:43:42 +0300

    organization completed with describe and stubbing added

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -1,11 +1,7 @@
-// const expect = require("chai").expect;
-
-// it("should add numbers correctly", () => {
-//   const num1 = 3;
-//   const num2 = 2;
-//   expect(num1 + num2).to.equal(5);
-// });
 const expect = require("chai").expect;
+const sinon = require("sinon");
+const jwt = require("jsonwebtoken");
+
 const authMiddleware = require("../middleware/is-auth");
 
 describe("Auth Middleware", () => {
@@ -28,5 +24,20 @@ describe("Auth Middleware", () => {
       }
     };
     expect(authMiddleware.bind(this, req, {}, () => {})).to.throw();
+  });
+
+  it("should yield a userId property after decoding the token", () => {
+    const req = {
+      get: headerName => {
+        return "Bearer ciciCat";
+      }
+    };
+    sinon.stub(jwt, "verify");
+    jwt.verify.returns({ userId: "ciciCat" });
+    authMiddleware(req, {}, () => {});
+    expect(req).to.have.property("userId");
+    expect(req).to.have.property("userId", "ciciCat");
+    expect(jwt.verify.called).to.be.true;
+    jwt.verify.restore();
   });
 });
